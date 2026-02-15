ee2bed8c764522f027a7891e0c45264c76160b7f
André de Paula Terceiro <andre@terceiro.com.br>
2026-02-15 19:41:03 -0300

    test: removing other unused imports

--- tests/01/aula-context/App.tsx
+++ tests/01/aula-context/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import UserScreen from './src/screens/UserScreen';
 
