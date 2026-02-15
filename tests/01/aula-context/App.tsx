d0e381e29e005a1d1237ac609f587f0030323f7d
André de Paula Terceiro <andre@terceiro.com.br>
2026-02-15 19:40:00 -0300

    test: removing an unused import

--- tests/01/aula-context/App.tsx
+++ tests/01/aula-context/App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import UserScreen from './src/screens/UserScreen';
