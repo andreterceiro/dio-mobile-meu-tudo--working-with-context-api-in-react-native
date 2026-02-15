f85736ac8bde1203fd49bef27ebe3b067f77ef67
André de Paula Terceiro <andre@terceiro.com.br>
2026-02-15 19:57:08 -0300

    test: completting the basic HomeScreen

--- tests/01/aula-context/src/screens/HomeScreen.tsx
+++ tests/01/aula-context/src/screens/HomeScreen.tsx
@@ -1,11 +1,28 @@
-import { View, TextInput } from "react-native";
+import { useState } from 'react';
+import { View, TextInput, Button } from "react-native";
 
-export default function HomeScreen() {
+import { StackNavigationProp } from '@react-navigation/stack';
+import { rootStackParamList } from '../../App';
+
+type HomeScreenProps = {
+    navigation: StackNavigationProp<rootStackParamList, "Home">
+}
+
+export default function HomeScreen({navigation}: HomeScreenProps) {
+    const [ inputText, setInputText ] = useState("")
+
+    const navigateToUserScreen = function () {
+        navigation.navigate("User", {username: inputText});
+    }
+    
     return (
         <View>
             <TextInput
                 placeholder="Digite seu nome..."
+                value={inputText}
+                onChangeText={setInputText}
             />
+            <Button title="Logar" onPress={navigateToUserScreen} />
         </View>
     )
 }
